356c9c24949ea9407c378031916eb9822a3a9eab
Daniel Morgan <danielrmorgan11@gmail.com>
2025-05-25 11:51:21 -0400

    pocketbase url

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,3 +1,3 @@
 import PocketBase from 'pocketbase';
 
-export const pb = new PocketBase('http://pocketbase-ywwck48084808wkg88kk00sk.162.55.223.194.sslip.io');
+export const pb = new PocketBase('https://pocketbase.danielmorgan.xyz');
