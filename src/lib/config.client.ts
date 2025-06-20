0824302f4049dca5e397103afe0b48068eebeb0b
Daniel Morgan <danielrmorgan11@gmail.com>
2025-06-19 22:58:40 -0400

    fixed pocketbase url

--- src/lib/config.client.ts
+++ src/lib/config.client.ts
@@ -2,7 +2,7 @@
 // At build time, build.ts injects POCKETBASE_URL into process.env.
 
 // Retrieve the injected PocketBase URL
-const _pbUrl = (window.location.origin.startsWith("http") ? 'http://127.0.0.1:8090' : 'https://pocketbase.danielmorgan.xyz');
+const _pbUrl = typeof window !== "undefined" ? (window.location.origin.startsWith("http") ? 'http://127.0.0.1:8090' : 'https://pocketbase.danielmorgan.xyz') : process.env.POCKETBASE_URL;
 if (!_pbUrl) {
     throw new Error('Missing environment variable: POCKETBASE_URL');
 }
